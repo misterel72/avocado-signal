429df2ec6ddeef0bdb83f61f6a4295807d384b28
misterel72 <misterel@duck.com>
2026-08-15 19:16:32 +0200

    Add search foundations and targeted landing pages

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,26 @@
+import type { APIRoute } from 'astro';
+
+const pages = [
+  '/',
+  '/services',
+  '/pricing',
+  '/about',
+  '/web-design-somerset',
+  '/websites-for-trades'
+];
+
+export const GET: APIRoute = ({ site }) => {
+  const baseUrl = site ?? new URL('https://avocadosignal.co.uk');
+  const urls = pages
+    .map((path) => `  <url>\n    <loc>${new URL(path, baseUrl).href}</loc>\n  </url>`)
+    .join('\n');
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls}
+</urlset>`;
+
+  return new Response(sitemap, {
+    headers: { 'Content-Type': 'application/xml; charset=utf-8' }
+  });
+};
